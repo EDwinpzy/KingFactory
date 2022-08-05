38e1e08360a58c97914f53a6a53a43360393077e
EDwinpzy <630548043@qq.com>
2022-08-05 17:01:13 +0800

    8.5

---
+++ V3.0/生产备库/子查父.js
@@ -0,0 +1,9 @@
+/**
+ * @type:
+ * @description:
+ * @return {*}
+ */
+var func = require('../生产备库/生成树.js');
+function childToFath(){
+    
+}
--- V3.0/生产备库/生成树.js
+++ V3.0/生产备库/生成树.js
@@ -3,10 +3,7 @@ var data = [
     { id: 2, name: '三偏心', parentId: 1 },
     { id: 3, name: '包装', parentId: 1 },
     { id: 4, name: '1#三偏心', parentId: 2 },
-    // { id: 5, name: '正极工厂', parentId: 0 },
-    // { id: 6, name: '除磁', parentId: 5 },
-    // { id: 7, name: '1#除磁', parentId: 6 },
-    // { id: 8, name: '2#除磁', parentId: 6 },
+    { id: 5, name: '2#三偏心', parentId: 4 },
 ];
 /**
  * @type:生成树数据结构
@@ -28,5 +25,6 @@ function transListToTreeData(list, rootValue) {
     });
     return arr;
 }
-var res = transListToTreeData(data, 0);
-console.log(res);
+modules.exports = {
+    transListToTreeData,
+};
